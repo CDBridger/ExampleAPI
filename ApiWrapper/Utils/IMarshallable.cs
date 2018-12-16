7cfddd576e45d61d2814469dc64979a0bc8cfe86
Akram Darwazeh <akramd@datacom.co.nz>
2018-12-17 11:50:23 +1300

    Even more comments

--- ApiWrapper/Utils/IMarshallable.cs
+++ ApiWrapper/Utils/IMarshallable.cs
@@ -5,6 +5,13 @@ using System.Text;
 
 namespace ApiWrapper.Utils
 {
+    /// <summary>
+    /// Classes that implement the IMarshallable interface can be passed into and out of unmanaged
+    /// memory. They must have a backing field which is a struct with the correct Marshallable 
+    /// primitives as fields. The fields must be decorated with the correct attributes for their
+    /// primitive types.
+    /// </summary>
+    /// <typeparam name="K">The type definition of the backing feild for this class</typeparam>
     public interface IMarshallable<K>
     {
         K BackingField { get; set; }
